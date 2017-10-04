e9622e41411d9637933cebeaec1b052294896b94
Nickolay Markov <nikolay.markov@komfo.com>
2017-10-04 17:42:15 +0300

    Created with create-react-app

---
+++ src/containers/app/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Home from '../home'
+import About from '../about'
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <p className="App-intro">
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </p>
+  </div>
+)
+
+export default App;
